60a046fa1eeda27832e68dd0cdc6d597edff9afc
ssyazilim <142086995+ssyazilim@users.noreply.github.com>
2026-06-10 11:44:43 +0300

    fix(no-ticket): bug fix

--- src/schemas/product-variant/validation.ts
+++ src/schemas/product-variant/validation.ts
@@ -66,7 +66,7 @@ export const VARIANT = (locale: ILocale = 'tr') => {
 export const ADD_VARIANT = (locale: ILocale = 'tr') => {
   return z.object({
     variantsType: VARIANTS_TYPE(locale),
-    variant: VARIANT(locale),
+    variants: z.array(VARIANT(locale)),
   });
 };
 export const ADD_VARIANTS = (locale: ILocale = 'tr') => z.array(ADD_VARIANT(locale));
